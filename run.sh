c338614b0515a27b0a6cb81a93a9873bcf4079a3
c568254965 <568254965@qq.com>
2019-07-23 18:17:10 +0800

    ..

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ mvn clean package -Dmaven.test.skip=true
 docker rm -f zipkin
 docker rmi -f zipkin:test
 docker build -t zipkin:test .
-docker run -d -p 6866:6866 --name zipkin zipkin:test java -Dspring.profiles.active=test -jar /app/app.jar
+docker run -d -p 6866:6866 --name zipkin zipkin:test java  -DSTORAGE_TYPE=elasticsearch -DES_HOSTS=http://140.143.227.51:9200  -Dspring.profiles.active=test -jar /app/app.jar
