f4306f0cb839b8664fd121bebd53cacfa749c708
c568254965 <568254965@qq.com>
2019-07-25 16:46:09 +0800

    .

--- src/main/java/com/huarui/Zipkin_App.java
+++ src/main/java/com/huarui/Zipkin_App.java
@@ -3,11 +3,9 @@ package com.huarui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
-import org.springframework.context.annotation.ComponentScan;
 import zipkin.server.internal.EnableZipkinServer;
 
 
