b587beebc337733625c0467bc984edffbd03dfe5
c568254965 <568254965@qq.com>
2019-07-26 16:33:36 +0800

    .

--- src/main/java/com/huarui/Zipkin_App.java
+++ src/main/java/com/huarui/Zipkin_App.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import zipkin.server.internal.EnableZipkinServer;
 
 
@@ -13,9 +12,10 @@ import zipkin.server.internal.EnableZipkinServer;
 @EnableAutoConfiguration
 @EnableZipkinServer
 //@EnableConfigServer
-@EnableHystrixDashboard
+//@EnableHystrixDashboard
 @EnableEurekaClient
-@EnableTurbine
+//@EnableTurbine
+@EnableZuulProxy
 public class Zipkin_App
 {
 	public static void main(String[] args)
